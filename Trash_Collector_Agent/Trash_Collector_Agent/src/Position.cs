de43f9065ae4d4f75462eba6b729590d83054a58
Bruno Bragança Mendes <bbmendes@gmail.com>
2017-09-11 00:53:04 -0300

    Added position class

---
+++ Trash_Collector_Agent/Trash_Collector_Agent/src/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trash_Collector_Agent.src
+{
+    class Position
+    {
+        public Int32 Line { get; set; }
+        public Int32 Column { get; set; }
+
+        public Position(Int32 line, Int32 column)
+        {
+            this.Line = line;
+            this.Column = column;
+        }
+    }
+}
